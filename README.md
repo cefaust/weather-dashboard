dbe56edb43b8c0f5848edcda57b1638587580982
Chris Faust <cefaust82@gmail.com>
2022-05-14 09:44:56 -0700

    adding changes to js,html

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # weather-dashboard
+
+## With this project I was tasked with using third-party APIs to allow users to access their data.  In this task I am going to create a weather dashboard that uses dynamically updated HTML and CSS.
+
+### The weather dashboard will include the following:
+
+1. Users will search for a city and then be returned the current and future conditions for that city.
+
+2. The city will then be added to a search history.
+
+3. The current conditions for that city will include city name, the date, an icon that represents the weather conditions, the temperature, humidity, wind speed, and the UV index.
+
+4. The future conditions will display the date, an icon that represents the weather conditions, the temperature, the wind speed, and the humidity.
+
+5. When the user clicks on a city in the search history then they will again be presented with the current and future conditions for that city.
+
+![]
+(gitHub pages)
---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+const OPEN_WEATHER_API_KEY = "2c3ea6a0e03a3e5e80c16d502c338408";
+const weatherUnit = "f";
+const speedUnit = "miles";
+
+$("#search-form").submit(handleFormSubmission);
+
+async function handleFormSubmission(event) {
+  event.preventDefault();
+  var inputs = $("#search-form :input");
+  var values = {};
+  inputs.each(function () {
+    values[$(this).attr("id")] = $(this).val();
+  });
+  let cityInput = values["search-input"];
+  let cityLatLong = await getLatLongFromCity(cityInput);
+  let weatherResponse = await getWeatherForCity(cityLatLong);
+  displayCurrentWeather(weatherResponse, cityLatLong.name)
+  console.log("weatherResponse", weatherResponse);
+}
+
+//TODO: take input and place in open weather geocoding api
+function getGeocodeApiUrl(city) {
+  return `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${OPEN_WEATHER_API_KEY}`;
+}
+
+async function getLatLongFromCity(cityInput) {
+  const API_URL = getGeocodeApiUrl(cityInput);
+  let apiResponse = await fetch(API_URL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw ["Some problem occurred", response.json()];
+      }
+
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    });
+
+  let latLongResponse = {
+    name: apiResponse[0].name,
+    lat: apiResponse[0].lat, 
+    long: apiResponse[0].lon
+  }; 
+  return latLongResponse;
+}
+//TODO: take  lat lon results and fetch from open weather one call api
+function getWeatherApiUrl(latLong) {
+  const exclude = ["minutely", "hourly"];
+  return `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.lat}&lon=${latLong.long}&exclude=${exclude.join(",")}&appid=${OPEN_WEATHER_API_KEY}`;
+}
+
+async function getWeatherForCity(latLong) {
+  const API_URL = getWeatherApiUrl(latLong);
+  let apiResponse = await fetch(API_URL)
+    .then(function (response) {
+      if (!response.ok) {
+        throw ["Some problem occurred", response.json()];
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    });
+  return apiResponse;
+}
+
+//TODO: The city will then be added to a search history.
+
+//TODo: add the search to local storage.
+
+//TODO: click on search history to display that city's weather.
+
+//TODO: display current weather to page
+
+//TODO:  The current conditions for that city will include city name, the date, an icon that represents the weather conditions, the temperature, humidity, wind speed, and the UV index.
+
+function displayCurrentWeather(weather, cityName){
+  // putting cityName on the page
+  var tempRounded = Math.round((getTemp(weather.current.temp) + Number.EPSILON) * 100) / 100;
+  var windRounded = Math.round((getWindSpeed(weather.current.wind_speed) + Number.EPSILON) * 100) / 100;
+  document.querySelector(".current-weather h1").innerHTML = `${cityName} (${ "date" }) <img src="foo">`;
+  document.querySelector(".current-weather span.temp").innerHTML = tempRounded;
+  document.querySelector(".current-weather span.wind").textContent = windRounded;
+  document.querySelector(".current-weather span humidity").textContent = weather.current.humidity;
+}
+
+//TODO: The future conditions will display the date, an icon that represents the weather conditions, the temperature, the wind speed, and the humidity.
+
+
+function getTemp(kelvin) {
+  switch (weatherUnit) {
+    case 'f':
+      return (kelvin - 273.15) * 9 / 5 + 32;
+    case 'c':
+      return kelvin - 273.15;
+  }
+  return kelvin;
+}
+
+function getWindSpeed(metersPerSec) {
+  if (speedUnit === "miles") {
+    return metersPerSec * 2.236936;
+  }
+  return metersPerSec;
+}
