34768a87c0888190ea2d47e36353b407894c7b2f
Chris Faust <cefaust82@gmail.com>
2022-05-14 16:41:14 -0700

    added iconand date to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const OPEN_WEATHER_API_KEY = "2c3ea6a0e03a3e5e80c16d502c338408";
 const weatherUnit = "f";
 const speedUnit = "miles";
+const weatherIcon ="https://openweathermap.org/img/wn/@2x.png"
 
 $("#search-form").submit(handleFormSubmission);
 
@@ -14,11 +15,11 @@ async function handleFormSubmission(event) {
   let cityInput = values["search-input"];
   let cityLatLong = await getLatLongFromCity(cityInput);
   let weatherResponse = await getWeatherForCity(cityLatLong);
-  displayCurrentWeather(weatherResponse, cityLatLong.name)
+  displayCurrentWeather(weatherResponse, cityLatLong.name);
   console.log("weatherResponse", weatherResponse);
 }
 
-//TODO: take input and place in open weather geocoding api
+
 function getGeocodeApiUrl(city) {
   return `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${OPEN_WEATHER_API_KEY}`;
 }
@@ -39,15 +40,19 @@ async function getLatLongFromCity(cityInput) {
 
   let latLongResponse = {
     name: apiResponse[0].name,
-    lat: apiResponse[0].lat, 
-    long: apiResponse[0].lon
-  }; 
+    lat: apiResponse[0].lat,
+    long: apiResponse[0].lon,
+  };
   return latLongResponse;
 }
-//TODO: take  lat lon results and fetch from open weather one call api
+
 function getWeatherApiUrl(latLong) {
   const exclude = ["minutely", "hourly"];
-  return `https://api.openweathermap.org/data/2.5/onecall?lat=${latLong.lat}&lon=${latLong.long}&exclude=${exclude.join(",")}&appid=${OPEN_WEATHER_API_KEY}`;
+  return `https://api.openweathermap.org/data/2.5/onecall?lat=${
+    latLong.lat
+  }&lon=${latLong.long}&exclude=${exclude.join(
+    ","
+  )}&appid=${OPEN_WEATHER_API_KEY}`;
 }
 
 async function getWeatherForCity(latLong) {
@@ -73,28 +78,37 @@ async function getWeatherForCity(latLong) {
 
 //TODO: display current weather to page
 
-//TODO:  The current conditions for that city will include city name, the date, an icon that represents the weather conditions, the temperature, humidity, wind speed, and the UV index.
+//TODO:  The current conditions for that city will include  the date, an icon that represents the weather conditions.
 
-function displayCurrentWeather(weather, cityName){
-  // putting cityName on the page
-  var tempRounded = Math.round((getTemp(weather.current.temp) + Number.EPSILON) * 100) / 100;
-  var windRounded = Math.round((getWindSpeed(weather.current.wind_speed) + Number.EPSILON) * 100) / 100;
-  document.querySelector(".current-weather h1").innerHTML = `${cityName} (${ "date" }) <img src="foo">`;
+function displayCurrentWeather(weather, cityName) {
+  var today = moment().format("M/D/YYYY")
+  var tempRounded =
+    Math.round((getTemp(weather.current.temp) + Number.EPSILON) * 100) / 100;
+  var windRounded =
+    Math.round(
+      (getWindSpeed(weather.current.wind_speed) + Number.EPSILON) * 100
+    ) / 100;
+  document.querySelector(
+    ".current-weather h1"
+  ).innerHTML = `${cityName} (${today})  <img src="https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png">`;
   document.querySelector(".current-weather span.temp").innerHTML = tempRounded;
-  document.querySelector(".current-weather span.wind").textContent = windRounded;
-  document.querySelector(".current-weather span humidity").textContent = weather.current.humidity;
+  document.querySelector(".current-weather span.wind").textContent =
+    windRounded;
+  document.querySelector(".current-weather span.humidity").textContent =
+    weather.current.humidity;
+  document.querySelector(".current-weather span.uv-index").textContent =
+    weather.current.uvi;
 }
 
 //TODO: The future conditions will display the date, an icon that represents the weather conditions, the temperature, the wind speed, and the humidity.
 
-// function DisplayFiveDayForcast
-
+// function DisplayFiveDayForecast(weather)
 
 function getTemp(kelvin) {
   switch (weatherUnit) {
-    case 'f':
-      return (kelvin - 273.15) * 9 / 5 + 32;
-    case 'c':
+    case "f":
+      return ((kelvin - 273.15) * 9) / 5 + 32;
+    case "c":
       return kelvin - 273.15;
   }
   return kelvin;
